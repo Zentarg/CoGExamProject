232ef7eb866700cc3db388e136aceaff99cc8702
Danish-Spots <55740290+Danish-Spots@users.noreply.github.com>
2019-12-09 17:53:55 +0100

    Added Account Settings VM and edited page

---
+++ MainWindow/ViewModels/AccountSettingsVM.cs
@@ -0,0 +1,42 @@
+﻿using MainWindow.Annotations;
+using MainWindow.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainWindow.ViewModels
+{
+    class AccountSettingsVM : INotifyPropertyChanged
+    {
+        private string _tempDisplayName;
+
+        public AccountSettingsVM()
+        {
+            AccountHandler.AccountList.LoadAccounts();
+
+        }
+
+        public string TempDisplayName
+        {
+            get { return _tempDisplayName; }
+            set
+            {
+                _tempDisplayName = value;
+            }
+        }
+
+
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
